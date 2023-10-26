f35a4075168721fb67f95622d4dd98b38e368f11
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-26 13:55:41 -0400

    React-Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import { useState } from "react";
 import ColorList from "./components/ColorList";
 import Form from "./components/Form";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 const App = () => {
+  // toast.success("🦄 Wow so easy!");
+  // toast.error("🦄 Wow so easy!");
+
+
   const [colors, setColors] = useState(new Values("#6ca7e2").all());
 
   const addColor = (color) => {
-    setColors(new Values(color).all());
+    try {
+      setColors(new Values(color).all())
+    } catch (error) {
+      toast.error(`Unable to parse color from string:  ${color}`)
+    }
+
   };
 
   return (
     <main>
       <Form addColor={addColor} />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
