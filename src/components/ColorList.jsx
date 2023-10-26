2f65175b1bddd03f07ec22c4aa18af272224b4ba
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-25 20:12:14 -0400

    create components

---
+++ src/components/ColorList.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const ColorList = () => {
+  return (
+    <div>ColorList</div>
+  )
+}
+
+export default ColorList
---
+++ src/components/SingleColor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleColor = () => {
+  return (
+    <div>SingleColor</div>
+  )
+}
+
+export default SingleColor
