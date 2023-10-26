b982b357466b22773258287c43c6bccee7555c94
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-26 14:09:33 -0400

    Clipboard

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,8 +1,17 @@
+import { toast } from "react-toastify";
+
 const SingleColor = ({ color }) => {
+  const copy = (hex) => {
+    console.log(hex);
+    navigator.clipboard.writeText(hex).then(() => {
+      toast(`${hex} copied to clipboard`);
+    });
+  };
   return (
     <article
       className={`color ${color.type === "tint" ? "false" : "color-light"}`}
       style={{ backgroundColor: color.hexString() }}
+      onClick={() => copy(color.hexString())}
     >
       <p className={"percent-value"}>{color.weight}</p>
       <p className={"color-value"}>{color.hexString()}</p>
